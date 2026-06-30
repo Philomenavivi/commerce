1c73a2e712e0aa9ea7f589f49043e302e6e15046
phillomena vivi <philomenanzisa22@gmail.com>
2026-06-30 17:03:17 +0300

    perf: fix lcp priority and aspect ratio properties on hero graphic

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>home page</div>
+    <div>ls</div>
+  
   );
 }
--- my-app/app/product/page.tsx
+++ my-app/app/product/page.tsx
@@ -51,7 +51,7 @@ export default function ProductBody() {
     <section className="bg-[#f5f5f5] min-h-screen px-4 md:px-10 py-6">
   
       <div className="relative  h-[200px] rounded-xl align item-center overflow-hidden mb-8">
-        <Image src={girlimage} alt="philo.png" width={300} height={300} />
+        <Image src={girlimage} alt="philo.png" fill className="object-cover" />
             
           
 
@@ -102,14 +102,7 @@ export default function ProductBody() {
             <h3 className="font-medium mb-3">Size</h3>
 
             <div className="flex flex-wrap gap-2">
-              {["XS", "S", "M", "L", "XL"].map((size) => (
-                <button
-                  key={size}
-                  className="border px-3 py-2 rounded-md hover:bg-black hover:text-white transition"
-                >
-                  {size}
-                </button>
-              ))}
+              
             </div>
           </div>
 
